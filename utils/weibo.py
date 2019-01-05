e4f210d4c3e4a54184dfa15f22f0a6742c45dbb6
puc_dong <38744096+1417766861@users.noreply.github.com>
2019-01-05 20:31:35 +0800

    Update weibo.py

--- utils/weibo.py
+++ utils/weibo.py
@@ -1,10 +1,10 @@
 #coding:utf-8
 
 authorize_url = 'https://api.weibo.com/oauth2/authorize'
-client_id = '649347796'
+client_id = 'your_client_id'
 redirect_uri= 'http://47.107.66.196/bindemail/'
 access_token_url = 'https://api.weibo.com/oauth2/access_token'
-client_secret = '9f54b7c596a363e91589c4845894cacf'
+client_secret = 'your_client_secret'
 info_url = 'https://api.weibo.com/2/users/show.json'
 share_url = 'https://api.weibo.com/2/statuses/share.json'
 friendships_url = 'https://api.weibo.com/2/friendships/friends.json'
